2bed197aa4bdb07dd15e96fb3102da02ec1cd7c4
soyoungkangme <soyoungkangme@gmail.com>
2022-06-29 16:49:28 +0900

    @SpringBootConfiguration 복습
    스프링-> 스프링부트
    - 살 = Service, DAO, Controller -> 내가 작성
    - 뼈 = 라이브러리들 -> 필요한거 찾아서 다운로드
    => 스프링 부트에 필요한 라이브러리 포함 , 버전/의존관계 관리  (스타터)
    - 연결 = xml -> 내가 작성
    => 스프링 부트가 제공 (자동설정 = 환경설정클래스의 빈등록으로 객체 로딩)
    <br>
    - 테스트 자동화 환경
    - 내장 서버 톰캣
    - 웹 프로젝트도 무거운 war(복잡함) 아닌 jar 로 패키징(압축) 가능

--- src/main/java/com/rubypaper/SpringBoot02Application.java
+++ src/main/java/com/rubypaper/SpringBoot02Application.java
@@ -9,23 +9,28 @@ import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 // @SpringBootApplication = 아래 3개 
-@SpringBootConfiguration  // @Configuration 과 동일 (환경설정 클래스) 
-@EnableAutoConfiguration  // 두번째 동작 : 스프링에 제공하는 객체 메모리 로딩 (HandlerMapping) = 자동설정 클래스 동작해서 메모리에 뜸 (스프링처럼 xml 빈등록 안해도됨)
-@ComponentScan            // 제일 먼저 동작 : 내가 만든 객체 메모리 로딩 (Controller)
+@SpringBootConfiguration  // = @Configuration (환경설정 클래스) 
+@EnableAutoConfiguration  // 두번째 동작 : 스프링이 제공하는 객체 메모리 로딩 (ViewResolver, HandlerMapping, DispatcherServlet 등) 
+@ComponentScan            // 제일 먼저 동작 : 내가 만든 객체 먼저 메모리 로딩 (Controller, DAO)
 public class SpringBoot02Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBoot02Application.class, args);   // 웹, 톰캣 라이브러리 추가 안하면 자바로만 실행 가능 -> pom.xml 에 web 스타터 추가 
 	}
-	
-	@Bean
-	public ViewResolver viewResolver() {
-		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-		viewResolver.setPrefix("/WEB-INF/board");
-		viewResolver.setSuffix(".jsp");
-		return viewResolver;
-	}
+
+
+    /* 스프링에서 제공하는 객체 내가 빈등록 안해도 @EnableAutoConfiguration(자동설정) 으로 메모리에 자동 로딩 (!= 스프링)
+    @Bean  // @Bean은 환경설정 클래스에서 사용 (컨테이너 생성시 메서드 실행)
+    ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/board");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+	*/
 
 }
 
-// 스프링 컨테이너 2개 -> 객체 두번 생성 (@EnableAutoConfiguration -> @EnableAutoConfiguration)
+// 스프링 컨테이너 2개 -> 객체 두번 생성(초기화) (@ComponentScan -> @EnableAutoConfiguration)
+
+// 스프링은 수십개의 자동설정 클래스 제공함 (AutoConfiguration 접미사로 붙은 클래스들) => 스프링처럼 환경설정 XML 안만듦 
